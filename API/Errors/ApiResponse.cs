e6da9aa8d788b192f2d8817deb4db4bfd4d91ef2
Ahmed Mohamed Taha <ahmed.Mtaha47@gmail.com>
2021-12-22 22:05:07 +0200

    End of section 5

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace API.Errors
+{
+
+public class ApiResponse
+{
+        public ApiResponse(int statusCode, string message=null)
+        {
+            StatusCode = statusCode;
+            Message = message??GetDefaultMessageForStatusCode(StatusCode);
+        }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch{
+                400 => "A bad request, you have made",
+                401 =>"Authrized, you are not",
+                404 =>"Resource found, it was not",
+                505 => "Errors are the path to the dark side. Erros lead to anger. Anger leads to hate. Hate lead to career change.",
+                _ => null
+
+            };
+        }
+
+        public int StatusCode { get; set; }
+    public string Message { get; set; }
+}
+
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
- 
+
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -16,6 +16,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Core.Interfaces;
 using API.Helpers;
+using API.Middleware;
+using API.Errors;
+using API.Extensions;
 
 namespace API
 {
@@ -31,32 +34,35 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository,ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x=> x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                // app.UseDeveloperExceptionPage();
+
+                app.UseSwaggerDocumentation();
             }
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-//config to serving static content
-app.UseStaticFiles();
+            //config to serving static content
+            app.UseStaticFiles();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
